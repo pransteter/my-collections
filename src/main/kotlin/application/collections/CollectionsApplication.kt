547d50d7c5fec8b07a65478348523ecdb9fd4c36
Romulo Pransteter da Silva <r.pransteter@gmail.com>
2022-11-17 22:02:05 -0300

    enhancement: add base files

---
+++ src/main/kotlin/application/collections/CollectionsApplication.kt
@@ -0,0 +1,11 @@
+package application.collections
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CollectionsApplication
+
+fun main(args: Array<String>) {
+	runApplication<CollectionsApplication>(*args)
+}
